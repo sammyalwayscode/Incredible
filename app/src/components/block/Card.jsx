5b43b917dda478ce77ac5958a02a8d6aefb3c20b
sammyalwayscode <olorundasamuel2@gmail.com>
2023-10-24 22:59:50 +0100

    Up���

---
+++ app/src/components/block/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div>This is My Card</div>;
+};
+
+export default Card;
---
+++ app/src/pages/Pricing/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      <h1>Your Best Price</h1>
+      <h2>$40</h2>
+    </div>
+  );
+};
+
+export default Pricing;
